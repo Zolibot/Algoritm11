7b953e70a0f4b31823063f943bb6ade0531701df
Alexander <piachev.a@ya.ru>
2023-04-02 17:41:22 +0800

    New revision

--- taskF/taskF.py
+++ taskF/taskF.py
@@ -1,23 +1,20 @@
-def is_palindrome(words):
-    max_index = len(words) - 1
-    for i in range(len(words)):
-        if words[i] == words[max_index]:
-            if i == max_index or i > max_index:
-                return True
-            max_index -= 1
-        else:
-            return False
+def is_palindrome(words: str) -> bool:
+    return words == words[::-1]
 
 
 def load_data() -> None:
-    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ """
+    punctuation: str = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ """
     file = open("/run/media/greger/Disk_E/GRENOW/Dev/sprints/Algoritms/taskF/input.txt", "rt")
-    data = file.read()
+    data: str = file.read()
     data = data.strip().translate(str.maketrans('', '', punctuation)).lower()
     print(is_palindrome(data))
 
 
+import timeit
+
+print(timeit.timeit("load_data()", number=1000, setup="from taskF import load_data"))
+
+
 if __name__ == '__main__':
     load_data()
-    # import timeit
-    # print(timeit.timeit("load_data()", number=1, setup="from __main__ import load_data"))
+
